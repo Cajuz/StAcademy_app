9f9f765e65d4cd0c63108fa7096367c39becf4cd
Cajuz <alvaroalvi57@gmail.com>
2025-02-26 20:19:09 -0300

    Fazendo botao de enviar link

--- app/src/main/java/br/com/etecia/myapp/RecuperarSenha_Activity.java
+++ app/src/main/java/br/com/etecia/myapp/RecuperarSenha_Activity.java
@@ -1,6 +1,10 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class RecuperarSenha_Activity extends AppCompatActivity {
+    MaterialToolbar idRecRetorno;
+    FloatingActionButton idFABBikeDoacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,22 @@ public class RecuperarSenha_Activity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        idRecRetorno = findViewById(R.id.RectopAppBar);
+        idRecRetorno.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Login_Activity.class));
+            }
+        });
+
+            idFABBikeDoacao = findViewById(R.id.btnRecuperarSenha);
+            idFABBikeDoacao.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getApplicationContext(),
+                            "Link enviado para seu email \n cheque sua caixa de entrada", Toast.LENGTH_SHORT).show();
+                }
+            });
+            return view;
+        }
     }
-}
